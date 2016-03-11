b8c243a33acdefb9ac3a673ab65280a6c3b9910f
daenerys <isralord_quin@hotmail.com>
2016-03-10 20:23:45 -0600

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fisrtAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the fisrtAppApp
+ */
+angular.module('fisrtAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fisrtAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fisrtAppApp
+ */
+angular.module('fisrtAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
